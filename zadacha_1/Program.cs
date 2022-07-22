7f02b975d19a570aa826c011f2a369da63858491
Olya <dolcheveleno@mail.ru>
2022-07-22 14:51:31 +0300

    z1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while ( index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(col[pos]);
+        pos++;
+    }
+}
+int [] array = new int[5];
+FillArray(array);
+PrintArray(array);
+if (array[0]==array[4] || array[1]==array[3])
+{
+    Console.WriteLine("Число - палиндром");
+}
+else
+{
+    Console.WriteLine("Число - не палиндром");
+}
